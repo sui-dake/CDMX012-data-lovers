13b1d8f8c74b3bcb3109d057cfea68473f22f00e
DenisseMolina <dmolinaruelas@gmail.com>
2022-02-21 17:12:39 -0600

    actualizacion

--- src/data.js
+++ src/data.js
@@ -1,9 +1,219 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export  const normal = (token) => {
+  if (token.type[0] == 'normal' ) {
+    return true;
+  } else if (token.type[1] == 'normal' ){
+    return true;
+  } 
+    return false;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export  const fire = (token) => {
+  if (token.type[0] == 'fire' ) {
+    return true;
+  } else if (token.type[1] == 'fire' ){
+    return true;
+  } 
+    return false;
 };
+
+export  const water = (token) => {
+  if (token.type[0] == 'water' ) {
+    return true;
+  } else if (token.type[1] == 'water' ){
+    return true;
+  } 
+    return false;
+};
+
+export  const electric = (token) => {
+  if (token.type[0] == 'electric' ) {
+    return true;
+  } else if (token.type[1] == 'electric' ){
+    return true;
+  } 
+    return false;
+};
+
+export  const grass = (token) => {
+  if (token.type[0] == 'grass' ) {
+    return true;
+  } else if (token.type[1] == 'grass' ){
+    return true;
+  } 
+    return false;
+};
+
+export  const ice = (token) => {
+  if (token.type[0] == 'ice' ) {
+    return true;
+  } else if (token.type[1] == 'ice' ){
+    return true;
+  } 
+    return false;
+};
+
+export  const fighting = (token) => {
+  if (token.type[0] == 'fighting' ) {
+    return true;
+  } else if (token.type[1] == 'fighting' ){
+    return true;
+  } 
+    return false;
+};
+
+export  const poison = (token) => {
+  if (token.type[0] == 'poison' ) {
+    return true;
+  } else if (token.type[1] == 'poison' ){
+    return true;
+  } 
+    return false;
+};
+
+export  const ground = (token) => {
+  if (token.type[0] == 'ground' ) {
+    return true;
+  } else if (token.type[1] == 'ground' ){
+    return true;
+  } 
+    return false;
+};
+
+export  const flying = function (token) { 
+  if (token.type[0] == "flying" ) {
+    return true;
+  }else if(token.type[1] == "flying" ){
+    return true;
+   }
+    return false;
+};
+
+export  const psychic = function (token) { 
+  if (token.type[0] == "psychic" ) {
+    return true;
+  }else if(token.type[1] == "psychic" ){
+    return true;
+    }
+    return false;   
+};
+
+export  const bug = function (token) { 
+  if (token.type[0] == "bug" ) {
+    return true;
+  }else if(token.type[1] == "bug" ){
+    return true;
+  }
+    return false; 
+};
+
+export  const rock = function (token) { 
+  if (token.type[0] == "rock" ) {
+    return true;   
+  }else if(token.type[1] == "rock" ){
+    return true;
+ }
+    return false;   
+};
+
+export  const ghost = function (token) { 
+  if (token.type[0] == "ghost" ) {
+    return true;
+  }else if(token.type[1] == "ghost" ){
+    return true;
+ }
+    return false;
+};
+
+export  const dragon = function (token) { 
+  if (token.type[0] == "dragon" ) {
+    return true;   
+  }else if(token.type[1] == "dragon" ){
+    return true;
+ }
+    return false; 
+};      
+
+export  const dark = function (token) { 
+  if (token.type[0] == "dark" ) {
+    return true;   
+  }else if(token.type[1] == "dark" ){
+    return true;
+   }
+    return false; 
+};  
+
+export  const steel = function (token) { 
+  if (token.type[0] == "steel" ) {
+    return true;   
+  }else if(token.type[1] == "steel" ){
+   return true;
+   }
+    return false;    
+};
+
+export  const fairy = function (token) { 
+  if (token.type[0] == "fairy" ) {
+    return true;   
+  }else if(token.type[1] == "fairy" ){
+    return true;
+  }
+    return false;   
+ };                   
+
+
+
+
+// WEAKNESSES //
+// export  const wfire = (token) => {
+//   if (token.type[0] == 'fire' ) {
+//     return true;
+//   } else if (token.type[1] == 'fire' ){
+//     return true;
+//   } 
+//   return false;
+// };
+
+/// ESTAS SI SIRVEN :)   ///
+
+export const sorting = (pkmn) => {
+  return pkmn.sort((a, b) => { 
+  if (a.num > b.num){
+  return -1;
+  } else if (a.num < b.num){
+    return 1;
+  }
+  return 0;
+  })} ;
+  
+export const sorting_asc = (pkmn) => {
+  return pkmn.sort((a, b) => { 
+  if (a.num > b.num){
+  return 1;
+  } else if (a.num < b.num){
+    return -1;
+  }
+  return 0;
+  })} ;
+
+export const sort_name_asc = (pkmn) => {
+  return pkmn.sort((a,b) => { 
+    if (a.name > b.name){
+      return 1;
+      } else if (a.name < b.name){
+        return -1;
+      }
+      return 0;
+      })} ;
+
+
+export const sort_name_dsc = (pkmn) => {
+  return pkmn.sort((a, b) => { 
+  if (a.name > b.name){
+  return -1;
+  } else if (a.name < b.name){
+    return 1;
+  }
+  return 0;
+  })} ;
+
+
--- src/main.js
+++ src/main.js
@@ -1,84 +1,167 @@
-import { example } from './data.js';
+import { sorting } from './data.js';
+import { sorting_asc } from './data.js';
+import { sort_name_asc } from './data.js';
+import { sort_name_dsc } from './data.js';
+import { ghost } from './data.js';
+import { fire } from './data.js';
+import { normal } from './data.js';
+import { water } from './data.js';
+import { electric } from './data.js';
+import { grass } from './data.js';
+import { ice } from './data.js';
+import { fighting } from './data.js';
+import { poison } from './data.js';
+import { ground } from './data.js';
+import { flying } from './data.js';
+import { psychic } from './data.js';
+import { bug } from './data.js';
+import { rock } from './data.js';
+import { dragon } from './data.js';
+import { dark } from './data.js';
+import { steel } from './data.js';
+import { fairy } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
-// bulbasaur.addEventListener("click", pokedex) ;
-
-// let pkm151 = data;
-// // document.getElementById('bulbasaur').innerHTML;
-// function pokedex () {
-//     return (pkm151.pokemon[0].name, bulbasaur);
-
-// }
-// ;
-
-// for(let i = 0; i < 10; i++){
-//      document.getElementById("bulbasaur").innerHTML 
-//     return (data.pokemon[i].name);
-// }
-
-        //// KANTO ////
-
-// let i = 0;
-// if (i == 0){
-//     let dex_kanto = '';
-//     while(i < 151){
-//         dex_kanto += `<img src="${data.pokemon[i].img}">`;
-//         i++
+// document.getElementById("Kanto").onclick = function() {
+//         document.getElementById("Kanto").style.display = "none";
 //     }
-//     document.getElementById("bulbasaur").innerHTML = dex_kanto;
-// } else if (i == 151){
-//     let dex_johto = '';
-//     while( i < 251){
-//         dex_johto += `<img src="${data.pokemon[i].img}">`;
-//         i++
+// document.getElementById("Johto").onclick = function() {
+//         document.getElementById("Johto").style.display = "none";
 //     }
-//     document.getElementById("bulbasaur2").innerHTML = dex_johto;
-// }
 
 
-    // for(let i = 0; i < 151; i++){
-    //     dex_kanto += `<img src="${data.pokemon[i].img}">`;
-    // }
-    
-
+        ///////////Obteniendo los pkm de Kanto en pantalla/////////////
     let dex = '';
-   // let dex3 = '';
+    // for(let i = 0; i < 10; i++){
+    //          dex += `<img src="${data.pokemon[i].img}">` +`${data.pokemon[i].name}`;
+            
+    //          let pikachu = document.getElementById("bulbasaur");        
+    //          pikachu.innerHTML = dex;
+             
+    //     }
+
+        ///////Obteniendo los pkm de Johto en pantalla//////
+    //let dex2 = '';
+    // for (let j = 151; j < 152; j++){
+    //         dex2 += `<img src="${data.pokemon[j].img}">`;
+    //         let pichu = document.getElementById("bulbasaur2");
+    //         pichu.innerHTML = dex2;
+    //     }
+     //  let normal2 = data.pokemon.filter(normal)
+
+
+
+const pikachu = document.getElementById("bulbasaur");
+const raichu = document.getElementById("A_Z");
+
+function drawPokemon (arreglo){
+    for(let i = 0; i < arreglo.length; i++){
+        dex += `<h2>${arreglo[i].name}</h2>
+        <p> <img src="${arreglo[i].img}"></p>`;
+
+        }
+ }
+
+pikachu.innerHTML=dex;
+
+raichu.addEventListener('change', (evento)=>{
+    let mew = '';
+    sort_name_asc(data.pokemon, evento.target.id ).forEach(mewtwo =>{
+        mew += drawPokemon(data.pokemon.filter(normal))
+    })
+    pikachu.innerHTML=mew;
+
+})
+
+const botones = document.querySelectorAll(".type_button");
+const cuandoSeHaceClick = function (evento) {
+if (evento.target.id == "normal"){
+    drawPokemon(data.pokemon.filter(normal))
+ 
+}else if (evento.target.id == "fire"){
+    drawPokemon(data.pokemon.filter(fire))
     
-   
-    for(let i = 0; i < 151; i++){
-      // if(i < 151 ){
-        
-        dex += `<img src="${data.pokemon[i].img}">` +`${data.pokemon[i].name} `;
-        
+}else if (evento.target.id == "water"){
+    drawPokemon(data.pokemon.filter(water))
+    
+}else if (evento.target.id == "electric"){
+    drawPokemon(data.pokemon.filter(electric))
+    
+}else if (evento.target.id == "grass"){
+    drawPokemon(data.pokemon.filter(grass))
+
+}else if (evento.target.id == "ice"){
+    drawPokemon(data.pokemon.filter(ice))
+}else if (evento.target.id == "fighting"){
+    drawPokemon(data.pokemon.filter(fighting))
+}else if (evento.target.id == "poison"){
+    drawPokemon(data.pokemon.filter(poison))
+}else if (evento.target.id == "ground"){
+    drawPokemon(data.pokemon.filter(ground))
+}else if (evento.target.id == "flying"){
+    drawPokemon(data.pokemon.filter(flying))
+}else if (evento.target.id == "psychic"){
+    drawPokemon(data.pokemon.filter(psychic))
+}else if (evento.target.id == "bug"){
+    drawPokemon(data.pokemon.filter(bug))
+}else if (evento.target.id == "rock"){
+    drawPokemon(data.pokemon.filter(rock))
+}else if (evento.target.id == "ghost"){
+    drawPokemon(data.pokemon.filter(ghost))
+}else if (evento.target.id == "dragon"){
+    drawPokemon(data.pokemon.filter(dragon))
+}else if (evento.target.id == "dark"){
+    drawPokemon(data.pokemon.filter(dark))
+}else if (evento.target.id == "steel"){
+    drawPokemon(data.pokemon.filter(steel))
+}else if (evento.target.id == "fairy"){
+    drawPokemon(data.pokemon.filter(fairy))
+}
+
+  //console.log(resultadoFiltro) 
+}
+ botones.forEach(boton => {
+    boton.addEventListener("click", cuandoSeHaceClick);
+})
+          //////Ordenando pokemones numerica y alfabeticamente////
+//document.getElementById('asc').addEventListener('click', sorting_asc)
+
+//       let sortd = `<img src="${data.pokemon.img}">`;
+//       document.getElementById('dsc').addEventListener('click', sorting(sortd));
+//       document.getElementById('dsc').innerHTML 
+      //  BULBASAUR.addEventListener('change', (e)=>{
+      //}).
+//         console.log(sortd)
+    
+//
+
           
-               let pikachu = document.getElementById("bulbasaur");
-        
-                pikachu.innerHTML = dex;
-    }
 
-            //  else {              
-        //         dex2 += `<img src="${data.pokemon[i].img}">`;
-        //         let pichu = document.getElementById("bulbasaur2");
-        //         pichu.innerHTML = dex2;
-           
-        // }
+//console.log(example, data);
+// //console.log((data.pokemon.type))
+// console.log((data.pokemon).filter(normal)); 
+// console.log((data.pokemon).filter(fire));  
+// console.log((data.pokemon).filter(water));
+// console.log((data.pokemon).filter(electric));
+// console.log((data.pokemon).filter(grass));
+// console.log((data.pokemon).filter(ice));
+// console.log((data.pokemon).filter(fighting));
+// console.log((data.pokemon).filter(poison));
+// console.log((data.pokemon).filter(ground));
+// console.log((data.pokemon).filter(flying));
+// console.log((data.pokemon).filter(psychic));
+// console.log((data.pokemon).filter(bug));
+// console.log((data.pokemon).filter(rock));
+// console.log((data.pokemon).filter(ghost));
+// console.log((data.pokemon).filter(dragon));
+// console.log((data.pokemon).filter(dark));
+// console.log((data.pokemon).filter(steel));
+// console.log((data.pokemon).filter(fairy));
+// console.table(sorting(data.pokemon));
+//console.table(sorting_asc(data.pokemon));
+//console.table(sort_name_asc(data.pokemon));
+//console.table(sort_name_dsc(data.pokemon));
 
-        // let dex2 = '';
-        // for (let j = 151; j < 251; j++){
-        //     dex2 += `<img src="${data.pokemon[j].img}">`;
-        //     let pichu = document.getElementById("bulbasaur2");
-        //     pichu.innerHTML = dex2;
-        // }
 
 
-
-//   if (data.pokemon<=data.pokemon - 151){
-//   document.getElementById("bulbasaur").innerHTML = dex_kanto;
-// } else{
-//     document.getElementById("bulbasaur2").innerHTML = dex_johto;
-// }
-
-console.log(example, data.pokemon[0].img);
-
-console.log(example, data);
